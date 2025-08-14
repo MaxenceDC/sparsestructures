888604806542c56f2d53f36adaf7a2855b673a47
maxence <maxdc7613@gmail.com>
2025-08-15 00:08:32 +0200

    Update to 1.21.8

--- forge/src/main/java/io/github/maxencedc/sparsestructures/SparseStructuresForge.java
+++ forge/src/main/java/io/github/maxencedc/sparsestructures/SparseStructuresForge.java
@@ -1,18 +1,17 @@
 package io.github.maxencedc.sparsestructures;
 
 import io.github.maxencedc.sparsestructures.command.DumpStructureSetsCommand;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class SparseStructuresForge {
 
-    public SparseStructuresForge() {
+    public SparseStructuresForge(FMLJavaModLoadingContext context) {
         SparseStructuresCommon.init();
-        IEventBus eventBus = MinecraftForge.EVENT_BUS;
-        eventBus.addListener(this::registerCommands);
+        RegisterCommandsEvent.BUS.addListener(this::registerCommands);
     }
 
     private void registerCommands(RegisterCommandsEvent event) {
